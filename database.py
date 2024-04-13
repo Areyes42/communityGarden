95652b8783a0392a22cdf767900c0393002e9d86
delster1 <delsterone@gmail.com>
2024-04-13 02:16:18 -0500

    found way to add stuff to dict

--- database.py
+++ database.py
@@ -2,10 +2,31 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 uri = "mongodb+srv://delsterone:jVYEYXmkXofN1jH8@cluster0.xf2gegz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1')) # this is what we will use to reference the database client
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+    
+collection = client['gettingStarted']['people']
+
+# create new documents
+peopleDocuments = [
+    {
+      "name": { "first": "Alan", "last": "Turing" },
+      "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+      "views": 1250000
+    }, 
+    {
+      "name": { "first": "Grace", "last": "Hopper" },
+      "contribs": [ "Mark I", "UNIVAC", "COBOL" ],
+      "views": 3860000
+    }
+]
+# insert documents
+result = collection.find_one({ "name.last": "Turing" })
+print(result)
+collection.insert_many(peopleDocuments)
+print(collection)
